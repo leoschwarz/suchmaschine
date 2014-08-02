4f0bffdede57e9a2b4b9022866bb63e48583e491
Leonardo Schwarz <leoschwarz@users.noreply.github.com>
2014-08-03 01:08:23 +0200

    Der Crawler verwendet nun eine Warteschlange.

--- bin/crawler.rb
+++ bin/crawler.rb
@@ -13,8 +13,43 @@ require './lib/html_parser.rb'
 
 
 module Crawler
-  def self.run_task
-    Task.fetch.callback { |task|
+  class Crawler
+    def initialize
+      @tasks = Queue.new
+      @loading_new_tasks = false
+    end
+    
+    def launch
+      puts "#{USER_AGENT} wurde gestartet."
+    
+      EventMachine.run {
+        # Warteschlange mit Aufgaben befüllen
+        Task.sample(TASK_QUEUE_SIZE).callback { |tasks|
+          tasks.each {|task| @tasks << task}
+          
+          # Timer, der dafür zu sorgen hat, dass die Warteschlange immer genug Aufgaben enthält.
+          EventMachine.add_periodic_timer(1) {
+            unless @loading_new_tasks
+              # Sobald weniger als 50% der maximal Anzahl an Aufgaben vorhanden ist, werden neue geladen.
+              if @tasks.length < TASK_QUEUE_SIZE / 2
+                @loading_new_tasks = true
+                Task.sample(TASK_QUEUE_SIZE).callback { |tasks|
+                  tasks.each {|task| @tasks << task}
+                  @loading_new_tasks = false
+                }
+              end
+            end
+          }
+          
+          # Start des Crawlens
+          PARALLEL_TASKS.times { do_next_task }
+        }
+      }
+    end
+    
+    def do_next_task
+      task = @tasks.pop
+      
       http_request = EventMachine::HttpRequest.new(task.encoded_url).get(timeout: 10)
       http_request.callback {
         header = http_request.response_header
@@ -27,13 +62,13 @@ module Crawler
               links.each {|link| Task.insert(URI.decode link)}
               task.store_result(html)
               
-              Crawler::run_task
+              do_next_task
             }
           else
             url = URLParser.new(task.encoded_url, header["location"]).full_path
             Task.insert(URI.decode(url))
 
-            Crawler::run_task
+            do_next_task
           end
         end
       
@@ -42,22 +77,12 @@ module Crawler
       http_request.errback {
         puts "[-] #{task.decoded_url}"
         
-        Crawler::run_task
+        do_next_task
       }
-    }    
-  end
-  
-  def self.launch
-    puts "#{config.user_agent} wurde gestartet."
-    
-    EventMachine.run {
-      10.times {
-        Crawler::run_task()
-      } 
-    }
+    end
   end
 end
 
 if __FILE__ == $0
-  Crawler::launch
+  Crawler::Crawler.new.launch
 end
