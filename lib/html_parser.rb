b0a5ddff07f38f0b202499d34ba56b6bb00cf19f
Leonardo Schwarz <leoschwarz@users.noreply.github.com>
2014-08-15 03:22:26 +0200

    Zwei neue Varianten zu HTMLParser hinzugefügt

--- lib/html_parser.rb
+++ lib/html_parser.rb
@@ -8,6 +8,7 @@ module Crawler
       @html = html
     end
     
+    # Variante nokogiri einfach
     def get_links
       doc = Nokogiri::HTML(@html)
       urls = []
@@ -19,5 +20,33 @@ module Crawler
       end
       urls
     end
+    
+    # Variante nokogiri traverse
+    #def get_links_2
+    #  doc = Nokogiri::HTML(@html)
+    #  urls = []
+    #  doc.traverse do |node|
+    #    if node.name == "a" and node.has_attribute? "href"
+    #      rel = node.attr("rel")
+    #      if rel.nil? or not rel.include? "nofollow" 
+    #        url = URLParser.new(@base_url, node.attr("href")).full_path
+    #        urls << url unless url.nil?
+    #      end
+    #    end
+    #  end
+    #  urls
+    #end
+    
+    # Variante ohne rel=nofollow Unterstützung
+    #def get_links_3
+    #  regex = /href\s*=\s*(\"([^"]*)\"|'([^']*)'|([^'">\s]+))/im
+    #  
+    #  @html.scan(regex).map{|result|
+    #    href = result[1] || result[2] || result[3]
+    #    URLParser.new(@base_url, href).full_path
+    #  }
+    #end
+    
+    
   end
 end
