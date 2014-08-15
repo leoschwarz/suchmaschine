be60f12be4fd212e55bbc52b71e818ea488c3b7f
Leonardo Schwarz <leoschwarz@users.noreply.github.com>
2014-08-15 16:40:04 +0200

    Kleine Anpassung beim ausführen der Tests

--- test/ts_all.rb
+++ test/ts_all.rb
@@ -1,10 +1,10 @@
+require_relative '../bin/crawler.rb'
 require 'minitest/autorun'
 require 'webmock/minitest'
 require 'eventmachine'
 
 ENV["LIGHTBLAZE_ENV"] = "test"
 
-require_relative '../bin/crawler.rb'
 require_relative 'tc_url_parser.rb'
 require_relative 'tc_robots.rb'
 require_relative 'tc_html_parser.rb'
