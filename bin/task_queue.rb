ea0179f3ce11b680ea8b09bcff24fbd244c4b006
Leonardo Schwarz <leoschwarz@users.noreply.github.com>
2014-08-29 12:16:10 +0200

    Die TaskQueue ist neu in einem eigenen Modul und Konfiguration wird nun geladen.

--- bin/task_queue.rb
+++ bin/task_queue.rb
@@ -1,28 +1,35 @@
 require 'socket'
 require_relative '../lib/task_queue/task_queue.rb'
+require_relative '../config/config.rb'
 
+load_configuration(TaskQueue, "task_queue.yml")
 
 module TaskQueue
   SERVER_PUT = "p"
   SERVER_GET = "g"
+  
+  def self.run
+    queue = TaskQueue.new
+    socket = TCPServer.new 2051
+    loop do
+      client = socket.accept
+      action = client.recv(1)
+  
+      if action == TaskQueue::SERVER_PUT
+        url = client.recv(10000)
+        queue.insert url
+        client.close
+      elsif action == TaskQueue::SERVER_GET
+        client.write queue.fetch
+        client.close
+      else
+        client.close
+        raise "Unbekannte Aktion: #{action.inspect}"
+      end
+    end
+  end
 end
 
-
-queue = TaskQueue.new
-socket = TCPServer.new 2051
-loop do
-  client = server.accept
-  action = client.recv(1)
-  
-  if action == TaskQueue::SERVER_PUT
-    url = client.recv(10000)
-    queue.insert url
-    client.close
-  elsif action == TaskQueue::SERVER_GET
-    client.write queue.fetch
-    client.close
-  else
-    client.close
-    raise "Unbekannte Aktion: #{action.inspect}"
-  end
+if __FILE__ == $0
+  TaskQueue.run
 end
