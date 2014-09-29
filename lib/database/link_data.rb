1e6d49b8ec6aba1441503bce21995571757f3479
Leonardo Schwarz <leoschwarz@users.noreply.github.com>
2014-09-29 17:36:50 +0200

    Modul TaskQueue wurde zu Database umbenannt, erweitert und in vielen Bereichen verbessert.

---
+++ lib/database/link_data.rb
@@ -0,0 +1,15 @@
+module Database
+  class LinkData
+    def self.add(url, data)
+      # TODO
+    end
+    
+    def self.get(url)
+      # TODO
+    end
+    
+    def self.delete(url)
+      # TODO
+    end
+  end
+end
