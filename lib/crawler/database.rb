ff576acdf246541630827c90bfa0c5b456db7b69
Leonardo Schwarz <leoschwarz@users.noreply.github.com>
2014-09-29 19:57:31 +0200

    Kleiner Fehler

--- lib/crawler/database.rb
+++ lib/crawler/database.rb
@@ -5,7 +5,7 @@ module Crawler
     end
     
     def self.queue_fetch()
-      Task.new(URL.new self._run("QUEUE_FETCH")[0])
+      Task.new(URL.stored self._run("QUEUE_FETCH")[0])
     end
     
     # TODO LinkData
