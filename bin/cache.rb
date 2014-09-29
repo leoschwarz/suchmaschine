d45b298f3c3449bcdbd45f54f8bfa8c5686f9144
Leonardo Schwarz <leoschwarz@users.noreply.github.com>
2014-09-29 18:27:30 +0200

    Kleinste Korrekturen

--- bin/cache.rb
+++ bin/cache.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/env ruby
 require 'socket'
 require './config/config.rb'
 require './lib/cache/cache.rb'
--- lib/database/storage_ram.rb
+++ lib/database/storage_ram.rb
@@ -1,3 +1,5 @@
+require 'singleton'
+
 module Database
   class StorageRAM
     include Singleton
