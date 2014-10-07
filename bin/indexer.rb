73713eb714ff1e160a254c659b09b5def9a39bd6
Leonardo Schwarz <leoschwarz@users.noreply.github.com>
2014-10-07 18:24:44 +0200

    Indexierer funktionsfähig.

--- bin/indexer.rb
+++ bin/indexer.rb
@@ -1,23 +1,28 @@
 #!/usr/bin/env ruby
-
 require 'bundler/setup'
 require_relative '../lib/common/common.rb'
 require_relative '../lib/indexer/indexer.rb'
 
-require 'lz4-ruby'
-require 'oj'
-
 Common::load_configuration(Indexer, "indexer.yml")
 
 module Indexer
+  include Common::DatabaseClient
+  
   class Main
     def run
-      # TODO Das ist alles nur provisorisch
-      docinfo_id = Database.queue_fetch
-      doc_hash   = Oj.load(LZ4.uncompress(File.read("/mnt/sdb/suchmaschine/docinfo:#{docinfo_id}")))[:document_hash]
-      text       = Oj.load(LZ4.uncompress(File.read("db/keyval/doc:#{doc_hash}")))[:text]
+      10.times{ start_thread }
+    end
+    
+    def start_thread
+      Thread.new do
+        docinfo_hash = Indexer::Database.index_queue_fetch
+        docinfo      = Indexer::DocumentInfo.load(docinfo_hash)
+        doc          = Indexer::Document.load(docinfo.document_hash)
+        text         = doc.text
+        words        = text.gsub(/[^a-zA-ZäöüÄÖÜ]+/, " ").downcase.split(" ").uniq
       
-      words = text.gsub(/[^a-zA-ZäöüÄÖÜ]+/, " ").downcase.split(" ").uniq
+        Indexer::Database.index_append(words.map{|word| [word, doc.hash]})
+      end
     end
   end
 end
--- lib/indexer/indexer.rb
+++ lib/indexer/indexer.rb
@@ -1,2 +1,5 @@
+module Indexer
+end
+
 # Alle Dateien im Verzeichnis laden 
 Dir[File.join(File.dirname(__FILE__), './*.rb')].each {|file| require file }
