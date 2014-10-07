b7938ab5d677664c8d1352a3734af9dcaa2f8558
Leonardo Schwarz <leoschwarz@users.noreply.github.com>
2014-10-07 18:29:18 +0200

    Fehler in bin/indexer.rb behoben

--- bin/indexer.rb
+++ bin/indexer.rb
@@ -11,17 +11,24 @@ module Indexer
   class Main
     def run
       10.times{ start_thread }
+      
+      loop do
+        # Haupt-Thread "beschäftigen"
+        sleep 100
+      end
     end
     
     def start_thread
       Thread.new do
-        docinfo_hash = Indexer::Database.index_queue_fetch
-        docinfo      = Indexer::DocumentInfo.load(docinfo_hash)
-        doc          = Indexer::Document.load(docinfo.document_hash)
-        text         = doc.text
-        words        = text.gsub(/[^a-zA-ZäöüÄÖÜ]+/, " ").downcase.split(" ").uniq
+        loop do
+          docinfo_hash = Indexer::Database.index_queue_fetch
+          docinfo      = Indexer::DocumentInfo.load(docinfo_hash)
+          doc          = Indexer::Document.load(docinfo.document_hash)
+          text         = doc.text
+          words        = text.gsub(/[^a-zA-ZäöüÄÖÜ]+/, " ").downcase.split(" ").uniq
       
-        Indexer::Database.index_append(words.map{|word| [word, doc.hash]})
+          Indexer::Database.index_append(words.map{|word| [word, doc.hash]})
+        end
       end
     end
   end
