1f262887c8b3c06bacfbd58cfcade57d61a5b6b1
Leonardo Schwarz <leoschwarz@users.noreply.github.com>
2014-10-09 18:48:11 +0200

    Kleine Fehlerkorrektur

--- lib/database/index.rb
+++ lib/database/index.rb
@@ -1,7 +1,7 @@
 module Database
   class Index
     # Fügt docinfo_id zu einem existierendem Index File hinzu oder erstellt ein neues.
-    def append(word, doc_id)
+    def self.append(word, doc_id)
       file_path = File.join(Database.config.index.directory, "word:#{word}")
       File.open(file_path, "a") do |file|
         file.puts doc_id
