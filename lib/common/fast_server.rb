2290f94a04e39583233ccb02991e08d71abd09c3
Leonardo Schwarz <leoschwarz@users.noreply.github.com>
2014-10-09 18:54:23 +0200

    FastServer gibt nun alle Fehler weiter.

--- lib/common/fast_server.rb
+++ lib/common/fast_server.rb
@@ -13,11 +13,11 @@ module Common
       @on_stop    = proc{}
       @on_error   = proc{|error| raise error}
     end
-    
+
     def start
       # Callback aufrufen
       @on_start.call
-      
+
       # Server richtig starten
       @server = MulticlientTCPServer.new(@port, 5, false)
       puts "Server gestartet unter #{@host}:#{@port}."
@@ -28,13 +28,13 @@ module Common
             # Die Antwort generieren
             request  = conn.gets.strip
             response = @on_request.call(request)
-            
+
             # Das Resultat schreiben und die Verbindung schliessen
             conn.puts response
           else
             sleep 0.01
           end
-        rescue SystemCallError => e
+        rescue => e
           @on_error.call(e)
         rescue SystemExit, Interrupt
           puts "Server wird heruntergefahren..."
@@ -43,19 +43,19 @@ module Common
         end
       end
     end
-    
+
     def on_request(&block)
       @on_request = block
     end
-    
+
     def on_start(&block)
       @on_start = block
     end
-    
+
     def on_stop(&block)
       @on_stop = block
     end
-    
+
     def on_error(&block)
       @on_error = block
     end
