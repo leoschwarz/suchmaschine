2ebda56428209ae09d792aa0014bcf4c6bdd9020
Leonardo Schwarz <leoschwarz@users.noreply.github.com>
2014-10-13 21:07:21 +0200

    Hoffentlich ein bisschen schneller

--- util/remove-html-from-docs.rb
+++ util/remove-html-from-docs.rb
@@ -5,13 +5,25 @@ puts "Anzahl Dateien: #{files.size}"
 require 'oj'
 require 'lz4-ruby'
 
-files.each_with_index do |path, counter|
-  puts counter if counter % 1000 == 0
+
+queue = Queue.new
+files.each_with_index{|file, index| @queue << [file, index]}
+files = nil
+
+threads = 10.times.map do
+  Thread.new do
+    begin
+      while file,index = queue.pop(true)
+        puts counter if counter % 1000 == 0
   
-  data = Oj.load(LZ4.uncompress(File.read(path)))
-  data.delete(:html)
+        data = Oj.load(LZ4.uncompress(File.read(path)))
+        data.delete(:html)
   
-  File.open(path, "w") do |f|
-    f.write(LZ4.compress(Oj.dump(data)))
+        File.open(path, "w") do |f|
+          f.write(LZ4.compress(Oj.dump(data)))
+        end        
+      end
+    rescue ThreadError
+    end
   end
 end
