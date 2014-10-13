3a014a2f78589aeb42a112341d7ffcf67ff7ea40
Leonardo Schwarz <leoschwarz@users.noreply.github.com>
2014-10-13 21:37:48 +0200

    Skript korrigiert

--- util/remove-html-from-docs.rb
+++ util/remove-html-from-docs.rb
@@ -7,23 +7,33 @@ require 'lz4-ruby'
 
 
 queue = Queue.new
-files.each_with_index{|file, index| @queue << [file, index]}
+files.each_with_index{|file, index| queue << [file, index]}
 files = nil
 
 threads = 10.times.map do
   Thread.new do
     begin
-      while file,counter = queue.pop(true)
-        puts counter if counter % 1000 == 0
+      while (item = queue.pop(true))
+        path,counter = item
+	puts counter if counter % 1000 == 0
+  	
+        begin
+          data = Oj.load(LZ4.uncompress(File.read(path)))
+          data.delete(:html)
   
-        data = Oj.load(LZ4.uncompress(File.read(path)))
-        data.delete(:html)
-  
-        File.open(path, "w") do |f|
-          f.write(LZ4.compress(Oj.dump(data)))
-        end        
+          File.open(path, "w") do |f|
+            f.write(LZ4.compress(Oj.dump(data)))
+          end        
+        rescue
+          File.unlink(path)
+          puts "Datei gelöscht: #{path}"
+        end
       end
     rescue ThreadError
     end
   end
 end
+
+threads.map(&:join)
+
+
