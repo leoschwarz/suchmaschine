a95c53e4e5d4b8369c99271a2a888b312269d6ff
Leonardo Schwarz <leoschwarz@users.noreply.github.com>
2014-10-14 19:24:55 +0200

    Kleine Fehlerkorrektur

--- lib/common/common.rb
+++ lib/common/common.rb
@@ -2,5 +2,3 @@
 Dir[File.join(File.dirname(__FILE__), './*.rb')].each {|file| require file }
 
 require_relative './database_client/database_client.rb'
-
-Common::load_configuration(Common, "common.yml")
