0adadd70d155b0e7b6654ceb8728cb8d4f4fb69a
Leonardo Schwarz <leoschwarz@users.noreply.github.com>
2014-10-26 18:19:28 +0100

    Indexer zur Indexierung von Metadata-Files angepasst.

--- lib/indexer/task.rb
+++ lib/indexer/task.rb
@@ -2,35 +2,27 @@ require 'lz4-ruby'
 
 module Indexer
   class Task
-    def initialize(document_hash, text)
-      @document_hash = document_hash
-      @text          = text
+    def initialize(hash, metadata)
+      @hash     = hash
+      @metadata = metadata
     end
 
     # Eine neue Aufgabe laden.
-    def self.load(path)
-      document = Indexer::Document.deserialize(LZ4.uncompress(File.read(path)))
-      self.new(document.hash, document.text)
+    def self.load(hash)
+      metadata = Indexer::Metadata.deserialize(LZ4.uncompress(File.read(Config.paths.metadata + hash)))
+      self.new(hash, metadata)
     end
 
     # Die Aufgabe bearbeiten.
     def run
-      # Ein Array aller vorkommenden kleingeschriebenen Wörter erzeugen.
-      all_words = @text.gsub(/[^a-zA-ZäöüÄÖÜ]+/, " ").downcase.split(" ")
-
-      # Wörter ab einer Länge von 20 Zeichen werden auf die ersten 20 Zeichen reduziert.
-      # Alle Duplikate entfernen...
-      all_words = all_words.map{|word| word[0...20]}.uniq
-
-      # Wörter in temporäre Dateien schreiben.
-      all_words.each do |word|
-        index_tmp_file(word).write_entries([@document_hash])
+      @metadata.word_counts.each_pair do |word, count|
+        postings_tmp(word).write_entries([@hash, count])
       end
     end
     
     private
-    def index_tmp_file(word)
-      Common::IndexFile.new(File.join(Config.paths.index_tmp, "word:#{word}"))
+    def postings_tmp(word)
+      Common::PostingsFile.new(File.join(Config.paths.index_tmp, "word:#{word}"))
     end
   end
 end
