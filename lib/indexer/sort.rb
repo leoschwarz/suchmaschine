6528d872f7284e5ebf3195415275e30c1a0c19d4
Leonardo Schwarz <leoschwarz@users.noreply.github.com>
2014-11-01 22:41:42 +0100

    Kleine Korrekturen

--- lib/indexer/sort.rb
+++ lib/indexer/sort.rb
@@ -9,7 +9,7 @@ require 'securerandom'
 # TODO: Noch einmals überarbeiten und aufräumen.
 
 module Indexer
-  class IndexFileChunk
+  class PostingsFileChunk
     # Die maximale Anzahl Reihen eines Dokumentes die geladen werden soll.
     # Dies gilt für *ein* Stück/Chunk.
     # Jede Reihe ist 20B gross, sodass sich für 500'000 Reihen 10MB ergeben.
@@ -17,15 +17,15 @@ module Indexer
     
     attr_reader :rows
     
-    def initialize(index_file, chunk_index)
-      @index_file  = index_file
+    def initialize(postings_file, chunk_index)
+      @postings_file  = postings_file
       @chunk_index = chunk_index
       @row_pointer = 0
     end
     
     # Erst wenn diese Methode aufgerufen wird, werden die Daten aus der Datei in den Arbeitspeicher gelesen.
     def load
-      @rows = @index_file.read_bin_entries(@chunk_index*MAX_ROWS, MAX_ROWS)
+      @rows = @postings_file.read_bin_entries(@chunk_index*MAX_ROWS, MAX_ROWS)
     end
     
     def shift_pointer
@@ -37,7 +37,7 @@ module Indexer
     end
   end
   
-  class IndexFileBufferedWriter
+  class PostingsFileBufferedWriter
     MAX_ROWS = 500_000
     
     def initialize(file)
@@ -54,7 +54,7 @@ module Indexer
     end
   end
   
-  class IndexFileChunkList
+  class PostingsFileChunkList
     def initialize(chunks)
       @chunks = chunks
       @chunks[0].load unless @chunks[0].nil?
@@ -80,11 +80,11 @@ module Indexer
     # Dies gilt für *ein* Stück/Chunk.
     # Jede Reihe ist 20B gross, sodass sich für 500'000 Reihen 10MB ergeben.
     MAX_ROWS = 500_000
-    ROW_SIZE = Common::IndexFile::ROW_SIZE
+    ROW_SIZE = Common::PostingsFile::ROW_SIZE
     # Die maximale Anzahl Stücke der grösse MAX_ROWS die geladen werden darf. 
     MAX_CHUNKS = 6
     
-    # Beide Parameter sollen Common::IndexFile Objekte sein.
+    # Beide Parameter sollen Common::PostingsFile Objekte sein.
     def self.sort(unsorted_file, destination_file)
       chunks = (unsorted_file.rows_count() *1.0 / MAX_ROWS).ceil
       
@@ -96,9 +96,9 @@ module Indexer
         destination_file.write_bin_entries(sorted_entries)
       else
         # In einem allerersten Schritt werden die einzelnen Chunks mit Dumbsort sortiert.
-        temp_file = Common::IndexFile.new(self.new_temp_file_path)
+        temp_file = Common::PostingsFile.new(self.new_temp_file_path)
         (0...chunks).each do |chunk_i| # TODO Später evtl einen speziellen Enumerator verwenden.
-          chunk = IndexFileChunk.new(temp_file, chunk_i)
+          chunk = PostingsFileChunk.new(temp_file, chunk_i)
           chunk.load
           temp_file.write_bin_entries( dumb_sort(chunk.rows) )
         end
@@ -116,17 +116,17 @@ module Indexer
     end
         
     # Sortiert die vorsortierten Stücke in source_file.
-    # source_file: Common::IndexFile
-    # output_file: Common::IndexFile
+    # source_file: Common::PostingsFile
+    # output_file: Common::PostingsFile
     # chunks_n: Integer, Anzahl Stücke in der Datei. Jeweils MAX_ROWS*ROW_SIZE gross, das letzte ist unter Umständen aber kleiner.
-    # chunk_range: Range, nil: Falls Range ein spezieller Indexbereich für den IndexFileChunks berücksichtigt werden sollen.
+    # chunk_range: Range, nil: Falls Range ein spezieller Indexbereich für den PostingsFileChunks berücksichtigt werden sollen.
     #                          Falls nil, dann wird die ganze Datei berücksichtigt.
     # TODO: Hier könnten noch ein paar böse Fehler lauern.
     def self.merge_sort(source_file, destination_file, chunks_n, chunk_range=nil)
       # Falls die Anzahl Stücke nun zu gross ist, müssen diese aufgeteilt werden
       if chunks_n > MAX_CHUNKS
         # Die maximal im RAM zu bearbeitende Anzahl Chunks liegt bei MAX_CHUNKS.
-        # Deshalb werden [MAX_CHUNKS] viele IndexFileChunkList Objekte erzeugt.
+        # Deshalb werden [MAX_CHUNKS] viele PostingsFileChunkList Objekte erzeugt.
         # Zuerst werden aber neue, leere Temporäre Dateien erzeugt, die durch Rekursion befüllt werden.
         # Index/Range-Berechnungen:
         sorted_subfiles_chunks_n = (0...MAX_CHUNKS).map do |index|
@@ -140,7 +140,7 @@ module Indexer
         end
         
         # Rekursion
-        sorted_subfiles = (0...MAX_CHUNKS).map{ Common::IndexFile.new(self.new_temp_file_path) }
+        sorted_subfiles = (0...MAX_CHUNKS).map{ Common::PostingsFile.new(self.new_temp_file_path, true) }
         sorted_subfiles.each_with_index do |subfile, index|
           range = sorted_subfiles_chunks_starts[index]...(sorted_subfiles_chunks_starts[index]+sorted_subfiles_chunks_n[index])
           self.merge_sort(source_file, subfile, sorted_subfiles_chunks_n[index], range)
@@ -148,14 +148,14 @@ module Indexer
         
         # Nun sind die jeweilgen Dateien der sorted_subfiles sortiert. Wir müssen lediglich noch diese zusammenlegen.
         # Da jedoch die einzelnen sorted_subfiles auch mehrere Chunks enthalten könnten, werden zur Sicherheit
-        # die Chunks in eine IndexFileChunkList geladen, die sicherstellt dass immer nur ein Chunk geladen ist.
+        # die Chunks in eine PostingsFileChunkList geladen, die sicherstellt dass immer nur ein Chunk geladen ist.
         chunk_lists = []
         sorted_subfiles.each_with_index do |subfile, index|
-          chunks = (0...sorted_subfiles_chunks_n[index]).map{|i| IndexFileChunk.new(subfile, i)}
-          chunk_lists << IndexFileChunkList.new(chunks)
+          chunks = (0...sorted_subfiles_chunks_n[index]).map{|i| PostingsFileChunk.new(subfile, i)}
+          chunk_lists << PostingsFileChunkList.new(chunks)
         end
         
-        result = IndexFileBufferedWriter.new(destination_file)
+        result = PostingsFileBufferedWriter.new(destination_file)
         while chunk_lists.size > 0
           # Minimum finden
           min_index = 0
@@ -187,9 +187,9 @@ module Indexer
       else
         # Die einzelnen Stücke laden und sortieren.
         chunk_range = 0...chunks_n if chunk_range.nil?
-        chunks = chunk_range.map{|i| IndexFileChunk.new(source_file, i)}
+        chunks = chunk_range.map{|i| PostingsFileChunk.new(source_file, i)}
         chunks.each{|chunk| chunk.load}
-        result = IndexFileBufferedWriter.new(destination_file)
+        result = PostingsFileBufferedWriter.new(destination_file)
         
         # Solange es noch nicht fertig gelesene Stücke hat, werden neue geladen.
         while chunks.size > 0
