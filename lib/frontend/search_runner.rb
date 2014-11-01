8314a206d50ed20e17a44fcb80b3ae716a0a853b
Leonardo Schwarz <leoschwarz@users.noreply.github.com>
2014-11-02 00:22:40 +0100

    Bei der Suche werden nicht gefundene Dateien (also Wörter) ignoriert anstat einen Fehler auszugeben

--- lib/frontend/search_runner.rb
+++ lib/frontend/search_runner.rb
@@ -19,11 +19,13 @@ module Frontend
         postings = Common::PostingsFile.new(word)
         postings_metadata = Common::PostingsMetadataFile.new(word)
         
-        postings_metadata.read
-        corpus_count = postings_metadata.total_occurences
+        if postings.exist?
+          postings_metadata.read
+          corpus_count = postings_metadata.total_occurences
         
-        postings.read_entries().each do |docid, count|
-          results[docid] += Math.log( count*1.0 / corpus_count )
+          postings.read_entries().each do |docid, count|
+            results[docid] += Math.log( count*1.0 / corpus_count )
+          end
         end
       end
       
