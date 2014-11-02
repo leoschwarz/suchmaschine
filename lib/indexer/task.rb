cf51c948613c185759cf3a91326280702217af2c
Leonardo Schwarz <leoschwarz@users.noreply.github.com>
2014-11-02 19:29:42 +0100

    Der Indexer schreibt nun nicht mehr jede Zeile einzeln sondern bündelt mehrere Writes zu einem indem ein LRUCache verwendet wird (aber nur maximal 200 Zeilen im RAM bleiben)

--- lib/indexer/task.rb
+++ lib/indexer/task.rb
@@ -1,28 +1,51 @@
 require 'lz4-ruby'
 
 module Indexer
+  class PostingsTmp
+    def initialize(word)
+      @word
+      @items = []
+    end
+    
+    def add(hash, count)
+      write if @items.size > 200
+      @items << [hash, count]
+    end
+    
+    def write
+      Common::PostingsFile.new(File.join(Config.paths.index_tmp, "word:#{@word}"), true).write_entries(@items)
+      @items.clear
+    end
+    
+    def removed_from_cache
+      write
+    end
+  end
+  
   class Task
-    def initialize(hash, metadata)
+    def initialize(hash, metadata, postings_tmps)
       @hash     = hash
       @metadata = metadata
+      @postings_tmps = postings_tmps
     end
 
     # Eine neue Aufgabe laden.
-    def self.load(hash)
+    def self.load(hash, postings_tmps)
       metadata = Indexer::Metadata.deserialize(LZ4.uncompress(File.read(Config.paths.metadata + hash)))
-      self.new(hash, metadata)
+      self.new(hash, metadata, postings_tmps)
     end
 
     # Die Aufgabe bearbeiten.
     def run
       @metadata.word_counts.each_pair do |word, count|
-        postings_tmp(word).write_entries([[@hash, count]])
+        if @postings_tmps.include?(word)
+          postings_tmp = @postings_tmps[word]
+        else
+          postings_tmp = PostingsTmp.new(word)
+          @postings_tmps[word] = postings_tmp
+        end
+        postings_tmp.add(@hash, count)
       end
-    end
-    
-    private
-    def postings_tmp(word)
-      Common::PostingsFile.new(File.join(Config.paths.index_tmp, "word:#{word}"), true)
     end
   end
 end
