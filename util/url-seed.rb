12d9119b970ddb5d2c6939f48d64eefe9d8e1791
Leonardo Schwarz <leoschwarz@users.noreply.github.com>
2014-11-09 16:46:41 +0100

    URL Seed Skript aktualisiert

--- util/url-seed.rb
+++ util/url-seed.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
-require_relative '../lib/crawler/crawler'
-
 # Dieses Skript lädt einige Startpunkte für die Websuche
+require_relative '../lib/crawler/crawler'
+require 'oj'
+
 # Deutschsprachige Seiten:
 $domains  = %w[de.wikipedia.org tagesanzeiger.ch srf.ch 20min.ch nzz.ch spiegel.de chip.de heise.de]
 $domains += %w[focus.de blick.ch bluewin.ch blogspot.ch sbb.ch local.ch comparis.ch]
@@ -10,6 +11,22 @@ $domains += %w[gutefrage.net mozilla.org yahoo.de]
 # Englischsprachige Seiten:
 $domains += %w[en.wikipedia.org stackoverflow.com imdb.com forbes.com cnn.com bbc.com theguardian.com]
 
+# Funktion um die URLs zu speichern.
+def insert_urls(urls)
+  urls.uniq.each_slice(50) do |_urls|
+    Crawler::Database.download_queue_insert(_urls)
+  end
+end
+
+
+# Überprüfen ob eine Cachedatei existiert
+CACHE_FILE = File.join(File.dirname(__FILE__), "..", "tmp", "url-seed.json")
+if File.exist? CACHE_FILE
+  puts "URLs werden aus der CACHE-Datei geladen..."
+  urls = Oj.load(File.read(CACHE_FILE))
+  insert_urls(urls)
+  Kernel.exit
+end
 
 # URLs für jede Seite laden
 def fetch_urls(url, retries=3)
@@ -28,9 +45,11 @@ def fetch_urls(url, retries=3)
   end
 end
 
+# URLs in Datenbank speichern
 all_urls = $domains.map{|domain| fetch_urls(Common::URL.encoded("http://#{domain}/"))}.flatten
+insert_urls all_urls
 
-# URLs in Datenbank eintragen
-all_urls.uniq.each_slice(50) do |urls|
-  Crawler::Database.download_queue_insert(urls)
+# URLs in die CACHE-Datei schreiben.
+File.open(CACHE_FILE, "w") do |file|
+  file.write(Oj.dump(all_urls))
 end
