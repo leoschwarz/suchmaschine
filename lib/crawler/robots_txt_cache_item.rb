fa216cd02a71be9da0db5ea52d467ba20e84540c
Leonardo Schwarz <leoschwarz@users.noreply.github.com>
2014-11-23 21:51:07 +0100

    Konfigurationsabschnitt "robotstxt" überarbeitet

--- lib/crawler/robots_txt_cache_item.rb
+++ lib/crawler/robots_txt_cache_item.rb
@@ -5,19 +5,19 @@ module Crawler
     fields :domain, :rules, :valid_until
 
     def save
-      Crawler::Database.cache_set("robotstxt:#{self.domain}", self.serialize)
+      Crawler::Database.cache_set("robotstxt:#{self.domain}", self.serialize) if Config.robotstxt.cache.enabled
     end
 
     # Sucht nach einem robots.txt Cache-Eintrag für eine bestimmte Domain.
     def self.load(domain)
-      item = self.deserialize(Crawler::Database.cache_get("robotstxt:#{domain}"))
+      item = self.deserialize(Crawler::Database.cache_get("robotstxt:#{domain}")) if Config.robotstxt.cache.enabled
       item = self.new({domain: domain}) if item.nil?
       item
     end
 
     def set_valid_for(seconds)
       if seconds == :default
-        self.valid_until = Time.now.to_i + Config.robotstxt.cache_lifetime
+        self.valid_until = Time.now.to_i + Config.robotstxt.cache.lifetime
       else
         self.valid_until = Time.now.to_i + seconds
       end
