e0cc94d8c39bca2b6ed4e548d486ad237dce88ce
Leonardo Schwarz <leoschwarz@users.noreply.github.com>
2014-11-26 19:00:49 +0100

    Kleines Statistikskript hinzugefügt

---
+++ util/db-stats.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+load( File.join(File.basename(__FILE__), "..", "bin", "database") )
+
+kb = Config.database.block_size.metadata
+options = {}
+options[:create_if_missing] = true
+options[:compression]       = LevelDBNative::CompressionType::SnappyCompression
+options[:block_size]        = kb * 1024
+options[:write_buffer_size] = 16 * 1024*1024
+data_store = LevelDBNative::DB.new(Config.paths.metadata, options)
+
+domain_counts = Hash.new(0)
+data_store.keys.each do |key|
+  metadata = Database::Metadata.deserialize(data_store[key])
+  domain   = (metadata.url).split("/")[0]
+  @domain_counts[domain] += 1
+end
+
+total = 0
+domain_counts.sort_by{|k,v| v}.reverse.each do |domain, value|
+  puts "#{domain} => #{value}"
+  total += value
+end
+puts "="*20
+puts "TOTAL: #{total}"
