2ed43973a05111a63c1bc5f6b48b5f0dd5015d0d
Leonardo Schwarz <leoschwarz@users.noreply.github.com>
2014-11-26 16:23:08 +0100

    URL aufgeräumt

--- lib/common/url.rb
+++ lib/common/url.rb
@@ -1,10 +1,22 @@
 require 'uri'
 
 module Common
+  # Diese Klasse abstrahiert (und vereinfacht) die Handhabung von URLs.
+  # Generell werden hier drei verschiedene Formen von URLs unterschieden:
+  # - encoded: Kodierte URLs in denen keine Sonderzeichen vorkommen, sie beginnen mit dem Schema.
+  # - decoded: Dekodierte URLs in denen Sonderzeichen vorkommen, sie beginnen mit dem Schema.
+  # - stored: Kodierte URLs in denen keine Sonderzeichen vorkommen, sie beginnen ohne das Schema.
   class URL
+    # Handelt es sich um eine absolute URL?
     attr_reader :full_url
 
+    # Die Klassenmethode 'new' privat machen.
+    # Es soll immer einer der Konstruktoren encoded, decoded oder stored explizit verwendet werden.
     private_class_method :new
+    
+    # Erzeugt eine neue URL Instanz.
+    # @param [String] encoded_url Die kodierte URL mit dem Schema.
+    # @param [Boolean] full_url Handelt es sich um eine absolute URL?
     def initialize(encoded_url, full_url=true)
       @encoded_url = encoded_url
       @full_url    = full_url
@@ -12,79 +24,111 @@ module Common
 
     # URL mit kodierten Sonderzeichen
     # Beispiel: http://de.wikipedia.org/wiki/K%C3%A4se
+    # @return [String]
     def encoded_url
       @encoded_url
     end
 
     # URL mit dekodierten Sonderzeichen, also ein UTF-8 String
     # Beispiel: http://de.wikipedia.org/wiki/Käse
+    # @return [String]
     def decoded_url
       @_decoded_url ||= URI.decode @encoded_url
     end
 
     # URL die in der Datenbank gespeichert wird. Ohne URI-Schema, mit kodierten Sonderzeichen
     # Beispiel: de.wikipedia.org/wiki/K%C3%A4se
+    # @return [String]
     def stored_url
       @_stored_url ||= @encoded_url.gsub(%r{^https?://}, "")
     end
 
+    # Aliase
     alias :encoded :encoded_url
     alias :decoded :decoded_url
     alias :stored :stored_url
-
-    def domain_name
-      match = /https?:\/\/([a-zA-Z0-9\.-]+)/.match(self.encoded_url)
-      if not match.nil?
-        domain_name = match[1].downcase
-      else
-        nil
-      end
+    
+    # Entfernt den Fragmentbezeichner von der URL.
+    # @return [nil]
+    def remove_fragment_identifier
+      @_decoded_url = nil
+      @_stored_url  = nil
+      @encoded_url  = @encoded_url.split("#")[0]
     end
 
+    # Gibt den kodierten Domainnamen der URL zurück.
+    # @return [String, nil] Domain Name in Kleinbuchstaben
+    def domain
+      @url_parts ||= url_parts
+      return nil if @url_parts[0].nil?
+      @url_parts[0].downcase
+    end
+    
+    # Gibt den Pfad der URL zurück.
+    # @return [String, nil] Pfad beginnend mit "/"
+    def path
+      @url_parts ||= url_parts
+      return nil if @url_parts[1].nil?
+      return "/" if @url_parts[1].empty?
+      @url_parts[1]
+    end
+
+    # Neue URL-Instanz aus URL im "encoded" Format erstellen.
+    # @param [String] url Die kodierte URL mit Schema.
+    # @return [URL]
     def self.encoded(url)
       new(url)
     end
 
+    # Neue URL-Instanz aus URL im "decoded" Format erstellen.
+    # @param [String] url Die dekodierte URL mit Schema.
+    # @return [URL]
     def self.decoded(url)
       new(URI.encode url)
     end
 
+    # Neue URL-Instanz aus URL im "stored" Format erstellen.
+    # @param [String] url Die kodierte URL ohne Schema.
+    # @return [URL]
     def self.stored(url)
       new("http://"+url)
     end
 
+    # Wandelt die URL-Instanz in eine URI-Instanz um.
+    # @return [URI]
     def to_uri
       URI(encoded_url)
     end
-
-    # Evaluiert wohin die als Parameter gegebene URL führt,
-    # wenn man sich gerade bei der URL der Instanz befindet.
-    # Gibt eine neue URL Instanz oder nil zurück.
+    
+    # Interpretiert die als Parameter gegebene URL als Link der auf einer Webseite gefunden
+    # wurde und evaluiert die Ziel-URL. Falls es sich um eine relative handelt, wird sie
+    # an das Ende hinzugefügt, falls es sich um eine absolute handelt, wird die absolute zurückgegeben
+    # und falls es sich um etwas handelt das keine URL ist oder es andere Fehler gibt, wird nil zurückgegeben.
+    # @param [URL, String]
+    # @return [URL, nil]    
     def join_with(url)
-      if url.class == String
-        url = URL.from_unknown(url)
-      end
-      if url.nil? or url.full_url
-        return url
-      end
-
+      # Eventuelle Typkonvertierung des Parameters
+      url = URL.from_unknown(url) if url.class == String
+      
+      # Wir sind fertig, falls die URL nil ist oder es sich um eine absolute URL handelt.
+      return nil if url.nil?
+      return url if url.full_url?
+      
       # Die URIs zusammenfügen
       begin
         url = URI::join(self.to_uri, url.to_uri).to_s
-        # Den Fragmentidentifier von der URL entfernen (falls vorhanden)
-        hash_index = url.index("#")
-        if hash_index.nil?
-          URL.encoded url
-        else
-          URL.encoded url[0...hash_index]
-        end
+        URL.encoded(url)
       rescue
         nil
       end
     end
 
-    # Versucht aus einem String, dessen Typ ungeklärt ist, ein URL Objekt zu machen.
+    # Versucht einen String, dessen Typ ungeklärt ist, in ein URL Objekt zu konvertieren.
+    # Es werden nur encoded und decoded unterstützt.
     # Falls dies nicht gelingt wird stattdessen nil zurückgegeben.
+    # @param [String] string Der zu konvertierende String.
+    # @param [Boolean] second_try Soll ein zweiter Versuch erlaubt sein? (In dem Fall wird versucht die URL zu kodieren)
+    # @return [URL, nil]
     def self.from_unknown(string, second_try=true)
       # 1. Versuch: die URL enthält keine besonderen Sonderzeichen...
       begin
@@ -103,15 +147,21 @@ module Common
       end
 
       # 2. Versuch: die URL enthält kodierbare Sonderzeichen...
-      if second_try
-        begin
-          return self.from_unknown(URI.encode(string).to_s, false)
-        rescue
-          return nil
-        end
-      end
-
+      return self.from_unknown(URI.encode(string).to_s, false) if second_try
       nil
+    end
+    
+    private
+    # Teilt die kodierte URL in die Domain und den Pfad auf.
+    # @return [Array]
+    def url_parts
+      match = %r{^http[s]?://([a-zA-Z0-9\.-]+)(.*)}.match(url)
+      result = []
+      if match
+        [match[1], match[2]]
+      else
+        [nil, nil]
+      end
     end
   end
 end
