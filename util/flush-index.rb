40e1d36ffa90d59ab0823b04593bda62068a880c
Leonardo Schwarz <leoschwarz@users.noreply.github.com>
2014-11-29 15:27:49 +0100

    Indexlöschskript überarbeitet

--- util/flush-index.rb
+++ util/flush-index.rb
@@ -3,25 +3,6 @@
 load './bin/database'
 load './bin/indexer'
 
-options = {block_size: 8* 1024*1024, write_buffer_size: 16 *1024*1024, compression: LevelDBNative::CompressionType::SnappyCompression}
-
-# aus: lib/database/server.rb
-db_options = {}
-{document: 256, 
- metadata: 8,
-    cache: 8,
- postings: 256,
- postings_metadata: 8}.each_pair do |name, kb|
-  options = {}
-  options[:create_if_missing] = true
-  options[:compression]       = LevelDBNative::CompressionType::SnappyCompression
-  options[:block_size]        = kb * 1024
-  options[:write_buffer_size] = 16 * 1024*1024
-  db_options[name] = options # = LevelDBNative::DB.new(Config.paths[name], options)
-end
-
-
-
 print "Warnung, dieses Skript wird alle Postings (und zugehörige Metadaten) unwiederbringlich löschen.\n"
 print "Um fortzufahren, bitte 'index löschen' eintippen: "
 input = gets.strip
@@ -30,37 +11,31 @@ if input != "index löschen"
   Kernel.exit
 end
 
+begin
+  db = Database::Backend.new
+rescue => e
+  raise "Um den Indexierer ausführen zu können, darf die Datenbank nicht von einem anderen Prozess verwendet werden."
+end
+
 puts "Löschvorgang begonnen..."
 puts "Löschen aller Index-Dateien..."
-files  = Dir[Config.paths.postings_block + "*"]
-files += Dir[Config.paths.postings_metadata + "*"]
 
-total = files.count
+File.unlink(Config.paths.index)
+Dir[File.join(File.dirname(__FILE__), "..", "tmp", "index", "*")].each{|file| File.unlink(file)}
+Dir[Config.paths.index_queue+"*"].each{|file| File.unlink(file)}
 
+puts "Alte Dateien gelöscht, beginne die neue Warteschlange zu befüllen..."
+
+puts "Befüllen der Index-Warteschlange begonnen..."
+keys = db.datastore_keys(:metadata)
+total = keys.size
 counter = 0
-files.each do |file|
-  File.unlink file
-  counter += 1
-  print "\r[#{counter}/#{total}] gelöscht." if counter % 100 == 0
-end
-puts "\rLöschen aller Index Dateien erfolgreich."
-
-puts "Löschen der alten INDEX_QUEUE begonnen..."
-Dir[Config.paths.index_queue + "*"].each do |file|
-  File.unlink(file)
-end
-puts "Löschen der alten INDEX_QUEUE erfolgreich."
-
-puts "Befüllen der INDEX_QUEUE begonnen..."
-ids = LevelDBNative::DB.new(Config.paths.metadata, db_options[:metadata]).keys
-counter = 0
-total = ids.count
-queue = Database::BetterQueue.new(Config.paths.index_queue)
-ids.each do |id|
-  queue.insert(id)
+keys.each do |key|
+  db.queue_insert(:index, key)
   counter += 1
   print "\r[#{counter}/#{total}] eingefügt." if counter % 100 == 0
 end
 
-queue.save
+db.save
+
 puts "\rBefüllen beendet.                      "
