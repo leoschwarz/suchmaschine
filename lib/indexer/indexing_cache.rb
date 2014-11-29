62ef134191b7be3736d9798c54b6aaf895eff55d
Leonardo Schwarz <leoschwarz@users.noreply.github.com>
2014-11-29 16:34:12 +0100

    Kleine Fehlerkorrektur

--- lib/indexer/indexing_cache.rb
+++ lib/indexer/indexing_cache.rb
@@ -5,15 +5,18 @@ module Indexer
     def initialize(flush_directory)
       @data = {}
       @data_size = 0
+      @data_mutex = Mutex.new
       @flush_directory = flush_directory
       @flush_thread    = nil
       @flushes = 0
     end
     
     def add(word, freq, doc)
-      @data[word] ||= Array.new
-      @data[word] << [freq, doc]
-      @data_size += Common::IndexFile::ROW_SIZE * 1.5 # <- Ruby-Ineffizienz Heuristik
+      @data_mutex.synchronize do
+        @data[word] ||= Array.new
+        @data[word] << [freq, doc]
+        @data_size += Common::IndexFile::ROW_SIZE * 1.5 # <- Ruby-Ineffizienz Heuristik
+      end
       
       flush if @data_size >= MAX_SIZE
     end
@@ -23,18 +26,20 @@ module Indexer
         @flush_thread = Thread.new do
           file = Common::IndexFile.new(File.join(@flush_directory, @flushes.to_s)).writer
       
-          @data.sort_by{|word, rows| word}.each do |word, rows|
-            file.write_header(word, rows.size)
-            rows.sort_by{|freq, doc| freq}.reverse.each do |freq, doc|
-              file.write_row(freq, doc)
+          @data_mutex.synchronize
+            @data.sort_by{|word, rows| word}.each do |word, rows|
+              file.write_header(word, rows.size)
+              rows.sort_by{|freq, doc| freq}.reverse.each do |freq, doc|
+                file.write_row(freq, doc)
+              end
             end
+            @data.clear
+            @data_size = 0
           end
       
           file.flush
       
           @flushes += 1
-          @data.clear
-          @data_size = 0
         end
       else
         @flush_thread.join
