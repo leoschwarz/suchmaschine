fdd5b14fa192f903f97d51a4ec423a8594c7c66d
Leonardo Schwarz <leoschwarz@users.noreply.github.com>
2014-11-30 11:00:38 +0100

    Index-Datei Leser und Schreiber in separate Dateien geteilt und IndexFilePointerReader überarbeitet

---
+++ lib/common/index_file_pointer_reader.rb
@@ -0,0 +1,66 @@
+module Indexer  
+  class IndexFilePointerReader
+    BUFFER_SIZE = 250_000
+    
+    def initialize(file_path, file_size)
+      @file_path = file_path
+      @file_size = file_size
+      
+      @buffer    = []
+      @pointer   = 0  # <- wo muss in der datei weitergelesen werden um den buffer zu ergänzen
+    end
+    
+    def current
+      if @buffer.empty?
+        fetch_buffer
+        @buffer[0] = nil if @buffer.empty?
+      end
+      @buffer[0]
+    end
+    
+    def shift
+      @buffer.delete_at(0)
+      fetch_buffer if @buffer.empty?
+      @buffer[0]
+    end
+    
+    private
+    def fetch_buffer
+      if @pointer < @file_size
+        raw = IO.binread(@file_path, @pointer, BUFFER_SIZE)
+        
+        # Einlesen
+        pointer = 0
+        loop do
+          freq_raw = raw.byteslice(pointer, 4)
+          if freq_raw.nil? || freq_raw.bytesize != 4
+            break
+          end
+          
+          freq = freq_raw.unpack("g")[0]
+          freq = raw.byteslice(pointer, 4).unpack("g")[0]
+          if freq == 0.0
+            type = :header
+            bytes = IndexFile::HEADER_SIZE
+          else
+            type = :row
+            bytes = IndexFile::ROW_SIZE
+          end
+          
+          if pointer + bytes > raw.bytesize
+            break
+          end
+          
+          if type == :header
+            @buffer << raw.byteslice(pointer, bytes).unpack(IndexFile::HEADER_PACK)[1..2]
+          elsif
+            @buffer << raw.byteslice(pointer, bytes).unpack(IndexFile::ROW_PACK)
+          end
+          pointer += bytes
+        end
+        
+        @pointer += pointer
+      end
+    end
+  end
+end
---
+++ lib/common/index_file_writer.rb
@@ -0,0 +1,32 @@
+module Common
+  class IndexFileWriter
+    def initialize(path, size, buffer_max)
+      @path   = path
+      @size   = size
+      @buffer = ""
+      @buffer_max = buffer_max
+    end
+    
+    def write_header(word, n)
+      @buffer << [0, word, n].pack(IndexFile::HEADER_PACK)
+      flush if @buffer.bytesize > @buffer_max
+    end
+    
+    def write_row(freq, doc)
+      @buffer << [freq, doc].pack(IndexFile::ROW_PACK)
+      flush if @buffer.bytesize > @buffer_max
+    end
+    
+    def write_rows(pairs)
+      @buffer << pairs.flatten.pack(IndexFile::ROW_PACK * pairs.size)
+      flush if @buffer.bytesize > @buffer_max
+    end
+    
+    def flush
+      # @size <=> offset
+      IO.binwrite(@path, @buffer, @size)
+      @size += @buffer.bytesize
+      @buffer.clear
+    end
+  end
+end
