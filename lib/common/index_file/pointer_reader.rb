5b09696b33de3de0c7d5756ac12dad8115eb9f5b
Leonardo Schwarz <leoschwarz@users.noreply.github.com>
2014-12-01 20:11:10 +0100

    Effizienzoptimierung in IndexFile::PointerReader

--- lib/common/index_file/pointer_reader.rb
+++ lib/common/index_file/pointer_reader.rb
@@ -1,14 +1,15 @@
 module Common  
   module IndexFile
     class PointerReader
-      BUFFER_SIZE = 250_000
-    
       def initialize(file_path, file_size)
         @file_path = file_path
         @file_size = file_size
         
-        @buffer    = []
-        @pointer   = 0  # <- wo muss in der datei weitergelesen werden um den buffer zu ergänzen
+        # Der erste Header wird bereits jetzt gelesen
+        header   = IO.binread(@file_path, IndexFile::HEADER_SIZE, 0).unpack(IndexFile::HEADER_PACK)
+        @buffer  = [:header, header[1], header[2]]
+        @next_position = IndexFile::HEADER_SIZE
+        @next_rowcount = header[2]
       end
     
       def current
@@ -23,43 +24,33 @@ module Common
       end
     
       private
-      # Liest neue Einträge in den Puffer, sofern dies möglich ist.
+      # Liest den nächsten Abschnitt an Zeilen, und dann den nächsten Header ein. (falls einer existiert...)
       def fetch_buffer
-        if @pointer < @file_size
-          # Rohdaten
-          raw = IO.binread(@file_path, BUFFER_SIZE, @pointer)
-          
-          pointer = 0
-          loop do
-            # Falls wir bereits das Frequenz/Header-Markier Feld nicht laden können, wird diese Schleife abgebrochen.
-            freq_raw = raw.byteslice(pointer, 4)
-            break if freq_raw.nil? || freq_raw.bytesize != 4
-            
-            # Falls freq genau 0.0 ist, handelt es sich um einen Header.
-            freq = freq_raw.unpack("g")[0]
-            if freq == 0.0
-              type = :header
-              bytes = IndexFile::HEADER_SIZE
-            else
-              type = :row
-              bytes = IndexFile::ROW_SIZE
-            end
-          
-            # Falls das Ende des Schnipsel ausserhalb des Rohstrings liegt, wird diese Schleife abgebrochen.
-            break if pointer + bytes > raw.bytesize
-            
-            # Die jeweiligen Daten laden
-            if type == :header
-              _, word, n = raw.byteslice(pointer, bytes).unpack(IndexFile::HEADER_PACK)
-              @buffer << [:header, word, n]
-            elsif type == :row
-              freq, doc = raw.byteslice(pointer, bytes).unpack(IndexFile::ROW_PACK)
-              @buffer << [:row, freq, doc]
-            end
-            pointer += bytes
+        size_without_header        = IndexFile::ROW_SIZE*@next_rowcount
+        size_with_following_header = IndexFile::ROW_SIZE*@next_rowcount + IndexFile::HEADER_SIZE
+        if @next_position + size_with_following_header <= @file_size
+          # Wir können alles lesen, dh. am Ende gibt es einen Header um weiterzulesen...
+          raw = IO.binread(@file_path, IndexFile::size_with_following_header, @next_position)
+          rows = raw.byteslice(0, size_without_header).unpack(IndexFile::ROW_PACK*@next_rowcount)
+          rows.each do |row|
+            @buffer << [:row, rows[0], row[1]]
           end
-        
-          @pointer += pointer
+          header = raw.byteslice(size_without_header, IndexFile::HEADER_SIZE).unpack(IndexFile::HEADER_PACK)
+          @buffer << [:header, header[1], header[2]]
+          
+          @next_position += size_with_following_header
+          @next_rowcount  = header[2]
+        else
+          # Wir können nur die Zeilen lesen, dh. am Ende gibt es keinen Header zum weiterlesen...
+          raw = IO.binread(@file_path, size_without_header, @next_position)
+          
+          rows = raw.unpack(IndexFile::ROW_PACK*@next_rowcount)
+          rows.each do |row|
+            @buffer << [:row, rows[0], row[1]]
+          end
+          
+          @next_position += size_without_header + 1
+          @next_rowcount = 0
         end
       end
     end
